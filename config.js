c1cd44c11da242e0a10dab237c1c213c835582e4
Midouch <amorriahmed@gmail.com>
2026-08-20 14:56:32 +0200

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://ojgynjupgbigsonpyprr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ojgynjupgbigsonpyprr.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_4_L-qY1A87zTDjHshBPtUg_8FVQZwLT";
